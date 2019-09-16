ee641f6d1106162b966709eee74227fe9cbb8d3e
marcelvenegas <marcel.venega@gmail.com>
2019-09-16 15:14:58 +0000

    completo!

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,14 +5,40 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			clickedLight: null
+		};
+	}
 	render() {
+		console.log(this.state);
+		let redExtraClass = "";
+		if (this.state.clickedLight == "red") redExtraClass = "selected";
+
+		let yellowExtraClass = "";
+		if (this.state.clickedLight == "yellow") yellowExtraClass = "selected";
+		let greenExtraClass = "";
+		if (this.state.clickedLight == "green") greenExtraClass = "selected";
+
 		return (
 			<div>
 				<div id="trafficTop" />
 				<div id="contanier">
-					<div className="red light" />
-					<div className="yellow light" />
-					<div className="green light" />
+					<div
+						className={"red light " + redExtraClass}
+						onClick={() => this.setState({ clickedLight: "red" })}
+					/>
+					<div
+						className={"yellow light " + yellowExtraClass}
+						onClick={() =>
+							this.setState({ clickedLight: "yellow" })
+						}
+					/>
+					<div
+						className={"green light " + greenExtraClass}
+						onClick={() => this.setState({ clickedLight: "green" })}
+					/>
 				</div>
 			</div>
 		);
